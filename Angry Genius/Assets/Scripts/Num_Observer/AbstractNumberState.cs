5ddceded9323509876df96e3007e76c8a9bc3f24
chinmaydhamankar <chinmaydhamankar1@gmail.com>
2016-04-23 22:23:25 +0530

    closes #38

---
+++ Angry Genius/Assets/Scripts/Num_Observer/AbstractNumberState.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractNumberState  {
+	public abstract void attach(AbstractNumberObserver obj);
+	public abstract void detach(AbstractNumberObserver obj);
+	public abstract void notifyObservers();
+}
