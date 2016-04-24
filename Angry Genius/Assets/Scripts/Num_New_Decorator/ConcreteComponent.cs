af4c60f949efe05c08087cb8d60ef105905470ad
abhaypendawle <pendawleabhay@gmail.com>
2016-04-23 23:33:10 -0700

    closes #50

---
+++ Angry Genius/Assets/Scripts/Num_New_Decorator/ConcreteComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConcreteComponent :Component {
+
+	protected GameObject gameObject;
+
+	public ConcreteComponent(GameObject gameObject){
+
+		this.gameObject = gameObject;
+	}
+
+	public override void Operation()
+	{
+		Debug.Log("ConcreteComponent.Operation()");
+	}
+
+	public override GameObject getGameObject()
+	{
+		Debug.Log("ConcreteComponent.getGameObject()");
+		return gameObject;
+	}
+}
