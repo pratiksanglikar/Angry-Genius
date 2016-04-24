e52f0d3bcbfd7af32cd56e2e8d09eb2d5b90c0cc
shrutipatil3101 <shruti.patil3101@gmail.com>
2016-04-23 21:39:58 -0700

    Initial commit fo first command pattern

---
+++ Angry Genius/Assets/Scripts/Main_Menu_Command_Pattern/Invoker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Invoker : IInvoker{
+
+	private ICommand cls;
+	private ICommand st;
+
+	public Invoker(ICommand c,ICommand s)
+	{
+		cls = c;
+		st = s;
+	}
+
+	public void gameclose()
+	{
+		cls.execute ();
+	}
+	public void gamestart()
+	{
+		st.execute ();
+	}
+}
