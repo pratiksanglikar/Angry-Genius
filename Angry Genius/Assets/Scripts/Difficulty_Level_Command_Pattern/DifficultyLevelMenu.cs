d18a9226f5c721617ee084bdb51d396f2122b517
shrutipatil3101 <shruti.patil3101@gmail.com>
2016-04-30 00:25:31 -0700

    closes #59

---
+++ Angry Genius/Assets/Scripts/Difficulty_Level_Command_Pattern/DifficultyLevelMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DifficultyLevelMenu : MonoBehaviour {
+
+	public bool Easy = false;
+	public bool Medium = false;
+	public bool Hard = false;
+
+
+	void OnMouseUp() {
+		IDifficultyLevelSwitchable l = gameObject.AddComponent<Level>();
+		IDifficultyLevelCommand e = new EasyLevel (l);
+		IDifficultyLevelCommand m = new MediumLevel (l);
+		IDifficultyLevelCommand h = new HardLevel (l);
+
+		DifficultyLevelInvoker i = new DifficultyLevelInvoker (e, m, h);
+
+		if (Easy == true) {
+			i.setEasyLevel();
+			//Application.LoadLevel(1);
+			//i.gamestart ();
+		} else if (Medium == true) {
+			i.setMediumLevel();
+			//Application.LoadLevel(1);
+			//i.gameclose ();
+		} else {
+		
+			i.setHardLevel();
+			//Application.LoadLevel(1);
+		}
+		Application.LoadLevel(3);
+	}
+
+	void Update()
+	{
+		//quit game if escape key is pressed
+		if (Input.GetKey(KeyCode.Escape)) 
+		{ 
+			Application.Quit();
+		}
+	}
+
+	void OnMouseEnter()
+	{
+		//change text color
+		GetComponent<Renderer>().material.color=Color.red;
+	}
+
+	void OnMouseExit()
+	{
+		//change text color
+		GetComponent<Renderer>().material.color=Color.white;
+	}
+}
---
+++ Angry Genius/Assets/Scripts/Difficulty_Level_Command_Pattern/Level.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour,IDifficultyLevelSwitchable {
+
+	public static bool isEasy=false;
+	public static bool isMedium=false;
+	public static bool isHard=false;
+
+	public void easyLevel (){
+		isEasy = true;
+	}
+	public void mediumLevel (){
+		isMedium = true;
+		Debug.Log("Setting true");
+	}
+	public void hardLevel (){
+		isHard = true;
+	}
+}
