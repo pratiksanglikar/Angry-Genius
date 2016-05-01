e1a03895329ce0cd6003a5743c56ac5cc844b98f
shrutipatil3101 <shruti.patil3101@gmail.com>
2016-04-30 18:20:22 -0700

    Level1

---
+++ Angry Genius/Assets/Scripts/Difficulty_Level_Command_Pattern/EasyLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyLevel : IDifficultyLevelCommand {
+
+	private IDifficultyLevelSwitchable e;
+
+	public EasyLevel(IDifficultyLevelSwitchable es)
+	{
+		e = es;
+	}
+
+	public void execute_level()
+	{
+		e.easyLevel ();
+	}
+}
