2348342968cf577a91dac420405ffcdcb7c77dc6
shaileshyedge <yedgeshailesh@gmail.com>
2016-04-30 23:33:52 -0700

    closes #69

---
+++ Angry Genius/Assets/Scripts/Managers/AlphaTargetTextManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AlphaTargetTextManager : MonoBehaviour {
+
+	public static int target_number; 
+	Text text;     
+	void Awake ()
+	{
+		// Set up the reference.
+		text = GetComponent <Text> ();
+		
+		// Reset the score.
+		target_number = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		text.text = "TARGET: " + target_number;
+	}
+}
