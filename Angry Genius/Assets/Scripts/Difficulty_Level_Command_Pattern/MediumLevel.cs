8b7864aa147747526fb0ce5ab3fc0d48c9678e13
shrutipatil3101 <shruti.patil3101@gmail.com>
2016-04-30 18:22:24 -0700

    LevelMedium

---
+++ Angry Genius/Assets/Scripts/Difficulty_Level_Command_Pattern/MediumLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MediumLevel : IDifficultyLevelCommand {
+	
+	private IDifficultyLevelSwitchable e;
+	
+	public MediumLevel(IDifficultyLevelSwitchable es)
+	{
+		e = es;
+	}
+	
+	public void execute_level()
+	{
+		e.mediumLevel ();
+	}
+}
