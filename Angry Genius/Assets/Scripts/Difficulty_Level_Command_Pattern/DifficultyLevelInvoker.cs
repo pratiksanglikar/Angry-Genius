9b587ffd29232148d2f6ffb6b27a7f9cd0c91377
shrutipatil3101 <shruti.patil3101@gmail.com>
2016-04-30 18:21:36 -0700

    Levelinoker

---
+++ Angry Genius/Assets/Scripts/Difficulty_Level_Command_Pattern/DifficultyLevelInvoker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DifficultyLevelInvoker : IDifficultyLevelInvoker {
+
+	private IDifficultyLevelCommand easy, medium, hard;
+
+	public DifficultyLevelInvoker (IDifficultyLevelCommand e, IDifficultyLevelCommand m, IDifficultyLevelCommand h)
+	{
+		easy = e;
+		medium = m;
+		hard = h;
+	}
+
+	public void setEasyLevel()
+	{
+		easy.execute_level ();
+	}
+
+	public void setMediumLevel()
+	{
+		medium.execute_level ();
+	}
+
+	public void setHardLevel()
+	{
+		hard.execute_level ();
+	}
+}
