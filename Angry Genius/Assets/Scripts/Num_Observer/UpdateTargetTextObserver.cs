58bc9fe5dd6b61e4316c010dbd165ab1cbea882a
chinmaydhamankar <chinmaydhamankar1@gmail.com>
2016-05-01 05:59:17 +0530

    closes #60

---
+++ Angry Genius/Assets/Scripts/Num_Observer/UpdateTargetTextObserver.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateTargetTextObserver : ConcreteNumberObserver {
+
+	private int target_number = AlphaTargetTextManager.target_number;
+	
+	public UpdateTargetTextObserver(ConcreteAlphaState state){
+		base.state = state;
+	}
+	
+	public override void update_observer(){
+		int updated_state = base.state.getState ();
+		if (updated_state == AlphaTargetTextManager.target_number) {
+			AlphaTargetTextManager.target_number = PlayerMovement.gameStrategy.getTarget();
+			
+			NumTextGen alphaGen = new NumTextGenImpl();
+			
+			AlphaTargetTextManager.target_number = alphaGen.getTargetNumber();
+			NumberTextManager.current_addition = 0;
+			
+			
+		}else if(updated_state > AlphaTargetTextManager.target_number){
+			
+			/*AlphaTextGen alphaGen=new AlphaTextGenImpl();
+			
+			AlphaTargetTextManager.target_alpha_string=alphaGen.getTargetString();
+			
+			AlphaTextManager.alpha_string="";*/ // game over state... TODO
+			
+		}
+	}
+}
